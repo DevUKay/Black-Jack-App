7ccb8cd2a6d747b671f4d280d76450c6d3ffd200
Dev Ukay <kingsleykrest@gmail.com>
2022-09-22 05:29:28 +1400

    Upgrades

--- styles/New.js
+++ styles/New.js
@@ -12,69 +12,72 @@
 ///////////////////////////
 
 
-let  cards = [ ]
-let  sum = 0
-let  hasBlackJack = false;
-let  isAlive = false;
-let  message = ""
+// let  cards = [ ]
+// let  sum = 0
+// let  hasBlackJack = false;
+// let  isAlive = false;
+// let  message = ""
 
-let messageEl = document.getElementById("message-el")
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.getElementById("cards-el")
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.querySelector("#sum-el")
+// let cardsEl = document.getElementById("cards-el")
 
 
-function getRandomCard() {
-    let randomNumber = Math.floor( Math.random() * 14)
-    if (randomNumber < 10) {
-        return 10
-    } else if (randomNumber === 1) {
-        return 11
-    } else {
-        return randomNumber
-    }
-}
+// function getRandomCard() {
+//     let randomNumber = Math.floor( Math.random() * 14)
+//     if (randomNumber < 10) {
+//         return 10
+//     } else if (randomNumber === 1) {
+//         return 11
+//     } else {
+//         return randomNumber
+//     }
+// }
 
-function startGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+// function startGame() {
+//     isAlive = true
+//     let firstCard = getRandomCard()
+//     let secondCard = getRandomCard()
+//     cards = [firstCard, secondCard]
+//     sum = firstCard + secondCard
 
-    renderGame()
-}
+//     renderGame()
+// }
 
-function renderGame() {
+// function renderGame() {
+//     cardsEl.textContent = "Cards: "
 
-    cardsEl.textContent = "Cards: "
+// for (let i = 0; i < cards.length; i++) {
+//     cardsEl.textContent += cards[i] + " "
+// }
 
-for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " "
-}
+//     sumEl.textContent = "Sum: " + sum
 
-    sumEl.textContent = "Sum: " + sum
+//     if (sum <= 20) {
+//         message = 'Do you want to draw a new card? '
+//     } else if (sum === 21) {
+//         message = "You've got Blackjack! "
+//         hasBlackJack = true
+//     } else {
+//         message = "You're out of the game!"
+//         isAlive = false
+//     }
 
-    if (sum <= 20) {
-        message = 'Do you want to draw a new card? '
-    } else if (sum === 21) {
-        message = "You've got Blackjack! "
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
+//     messageEl.textContent = message
+// }
 
-    messageEl.textContent = message
-}
 
-function newCard() {
+// function newCard() {
 
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+//     if (isAlive === true && hasBlackJack === false) {
+//         let card = getRandomCard()
+//         sum += card
+//         cards.push(card)
+//         renderGame()
+//     }
+    
 
-}
+// }
 
 /////////////////////////
 
@@ -134,3 +137,14 @@ function newCard() {
 // console.log(flooredNumber)
 
 ////////////////////////////////////////
+
+
+
+let listings = {
+    roomType: "private",
+    price: 190,
+    Available: true,
+    specs: [ 'big', 'castle', 'grey', 'landlocked','natural']
+}
+
+console.log('listings.price')
